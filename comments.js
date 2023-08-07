4f86e65654ef2c4347bcd72097862a5b4c28f551
Mariano García Roy <marianogroy@gmail.com>
2023-08-07 08:59:10 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Create web server using Node.js
+// Create a folder called nodejs
+// Create a file called server.js
+// Add the following code:
+// var http = require("http");
+// http.createServer(function (request, response) {
+//     // Send the HTTP header
+//     // HTTP Status: 200 : OK
+//     // Content Type: text/plain
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     // Send the response body as "Hello World"
+//     response.end('Hello World\n');
+// }).listen(8081);
+// // Console will print the message
+// console.log('Server running at http://
